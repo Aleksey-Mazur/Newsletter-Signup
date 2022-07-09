9c95b9705af304403412c776aaffc6ab888ade4b
Aleksey <1aleksey.mazur@gmail.com>
2022-07-10 01:24:04 +0300

    html markup have been added into signup.html; required modules have been installed into app.js; configured interaction with mailchimp api to receive data from the form and send it to mailchimp email base

--- app.js
+++ app.js
@@ -1,13 +1,60 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const https = require("https");
 
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us10.api.mailchimp.com/3.0/lists/f222cc009d";
+
+  const options = {
+    method: "POST",
+    auth: "Aleksey1:31bc22542e103e115d249347123c5125-us10",
+  };
+
+  const request = https.request(url, options, (response) => {
+    response.on("data", (data) => {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}...`);
 });
+
+// api key
+// 31bc22542e103e115d249347123c5125-us10
+
+// list id
+// f222cc009d
